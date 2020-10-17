f25d4c99e933adb0a6e49ea569f35dfb50c3e249
Joey Rich <joeyfrich@gmail.com>
2020-10-16 23:14:15 -0400

    Added migration for file_resources & deleted auto-generated package files b/c package was not working.

---
+++ database/migrations/2020_10_17_014656_file_resources.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FileResources extends Migration
+{
+  public function up()
+  {
+    Schema::create('file_resources', function (Blueprint $table) {
+      $table->bigIncrements('resource_id');
+			$table->string('resource_type')->nullable();
+			$table->string('resource_key')->nullable();
+			$table->string('hash_identifier')->nullable();
+			$table->string('file_extension')->nullable();
+			$table->timestamps();
+    });
+  }
+
+  public function down()
+  {
+    Schema::dropIfExists('file_resources');
+  }
+}
--- packages/joeyfrich/imageresizer/src/imageresizer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace joeyfrich\imageresizer;
-
-class imageresizer
-{
-    // Build wonderful things
-}
