b4e5a85a140aa3971e70cc17704c7d8a62297ae9
Joey Rich <joeyfrich@gmail.com>
2020-10-16 21:01:16 -0400

    Added a JS page manager class to the upload image view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,34 @@
   Welcome to the ImageResizer demo page.<br/>
   To test out the image resizing service, please upload an image file.<br/>
 
-  <form method="post" action="/image" enctype="multipart/form-data" name="demoForm" style="margin-top: 10px;">
+  <form method="post" action="/image" enctype="multipart/form-data" name="demoForm" id="demoForm" style="margin-top: 10px;">
     <input type="hidden" name="_token" value="{{ csrf_token() }}" />
     <div class="form-group">
-      <input type="file" name="demoFile" class="btn btn-default" required="true" onchange="document.demoForm.submit()" />
+      <input type="file" name="demoFile" id="demoFile" class="btn btn-default" required="true" autocomplete="off" />
     </div>
   </form>
+  <script type="text/javascript">
+  function UploadPageManager(formElId, fileElId) {
+    this.formEl = document.getElementById(formElId);
+    this.fileEl = document.getElementById(fileElId);
+    
+    this.initialize = function() {
+      this.fileEl.addEventListener(
+         'change',
+         function() {this.submitForm()}.bind(this)
+      );
+    }
+    
+    this.submitForm = function() {
+      this.formEl.submit();
+    };
+    
+    this.initialize();
+  }
+  var thisPageManager;
+  
+  window.onload = function() {
+    thisPageManager = new UploadPageManager("demoForm", "demoFile");
+  }
+  </script>
 @endsection
