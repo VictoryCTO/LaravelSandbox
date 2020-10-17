d30ff709a65334d5a5b707ac1dc30a139d7c4f7c
Joey Rich <joeyfrich@gmail.com>
2020-10-16 21:22:36 -0400

    Moved UploadPageManager class into app.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,23 +11,6 @@
     </div>
   </form>
   <script type="text/javascript">
-  function UploadPageManager(formElId, fileElId) {
-    this.formEl = document.getElementById(formElId);
-    this.fileEl = document.getElementById(fileElId);
-    
-    this.initialize = function() {
-      this.fileEl.addEventListener(
-         'change',
-         function() {this.submitForm()}.bind(this)
-      );
-    }
-    
-    this.submitForm = function() {
-      this.formEl.submit();
-    };
-    
-    this.initialize();
-  }
   var thisPageManager;
   
   window.onload = function() {
