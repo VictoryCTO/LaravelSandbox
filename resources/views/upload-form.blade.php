df88383e25d0fc8781c0c1c95e116c61292b9714
Mark Pemburn <mark@pemburn.com>
2020-10-19 16:37:27 -0400

    Pretty-up the form.

--- resources/views/upload-form.blade.php
+++ resources/views/upload-form.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-md-12">
+    <div class="col-md-6">
         <form method="POST" action="{{ $formAction }}" enctype="multipart/form-data">
             @csrf
-            <input type="text" name="key_name">
-            <input type="file" name="upload_image">
-            <div>
-                <button type="submit" class="btn btn-primary">Upload</button>
+            <div class="form-group">
+                <label for="key_name">Give your image a name:</label>
+                <input type="text" class="form-control" id="key_name" name="key_name">
             </div>
+            <div class="form-group">
+                <label for="upload_image">Your file goes here:</label>
+                <input type="file" class="form-control" id="upload_image" name="upload_image">
+            </div>
+            <button type="submit" class="btn btn-primary">Upload</button>
         </form>
     </div>
 @endsection
