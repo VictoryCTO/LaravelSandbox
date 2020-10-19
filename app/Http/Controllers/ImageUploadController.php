c4bfa82ff6b06a9d3ea9b5963a29ece9ba8d1123
Mark Pemburn <mark@pemburn.com>
2020-10-19 09:44:44 -0400

    Create ImageUploadController

---
+++ app/Http/Controllers/ImageUploadController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ImageService;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View as ReturnView;
+use Illuminate\Support\Facades\View;
+
+class ImageUploadController extends Controller
+{
+    public function showForm(): ReturnView
+    {
+        return View::make('upload-form', ['formAction' => '/process']);
+    }
+
+    public function processForm(ImageService $imageService, Request $request): string
+    {
+        $this->validate($request, [
+            'upload_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $imageService->processUpload($request);
+
+        return 'Did it!';
+    }
+}
